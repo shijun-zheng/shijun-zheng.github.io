79350bb273f682fbab32e1828cdb4688b4bfad0e
shijun-zheng <szheng@georgiasouthern.edu>
2016-08-19 13:42:41 -0400

    all my pages

---
+++ Equation/images.pl
@@ -0,0 +1,13 @@
+# LaTeX2HTML 2K.1beta (1.48)
+# Associate images original text with physical files.
+
+
+$key = q/displaystylefrac{partialu}{partialt}=mathcal{L}u+mathcal{N}(u);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="124" HEIGHT="51" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$\displaystyle \frac{\partial u}{\partial t}
+= \mathcal{L} u+ \mathcal{N}(u)$">|; 
+
+1;
+
